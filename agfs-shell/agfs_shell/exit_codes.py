38066ad8d81fb6af24633123640f29b826434314
dongxu <i@huangdx.net>
2025-12-06 23:25:41 -0800

    add missing files

---
+++ agfs-shell/agfs_shell/exit_codes.py
@@ -0,0 +1,10 @@
+"""Special exit codes for shell control flow and internal signaling"""
+
+# Control flow exit codes (used by break/continue)
+EXIT_CODE_CONTINUE = -995  # Signal continue statement in loop
+EXIT_CODE_BREAK = -996     # Signal break statement in loop
+
+# Collection signal codes (used by REPL to collect multi-line constructs)
+EXIT_CODE_FOR_LOOP_NEEDED = -997      # Signal that for loop needs to be collected
+EXIT_CODE_IF_STATEMENT_NEEDED = -998  # Signal that if statement needs to be collected
+EXIT_CODE_HEREDOC_NEEDED = -999       # Signal that heredoc data needs to be read
